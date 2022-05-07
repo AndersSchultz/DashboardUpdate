14d32f25bc2e5d0118bffd1dcd2049980210e277
Anders Schultz <mail@anders-schultz.dk>
2022-05-07 19:57:23 +0100

    bug fix

--- api/index.js
+++ api/index.js
@@ -2,12 +2,11 @@ import { settings } from '../settings.mjs'
 import { getValuesFromTokenContract } from '../token-amounts.mjs';
 import BN from 'bn.js'
 
-var allTokenAmountsReceived
 const ethValues = await getValuesFromTokenContract(settings.Ethereum.TokenAddresses, 'Ethereum')
 const polygonValues = await getValuesFromTokenContract(settings.Polygon.TokenAddresses, 'Polygon')
-allTokenAmountsReceived = ethValues.length > 0 && polygonValues.length > 0
-var output = { success: allTokenAmountsReceived }
-
+var allTokenAmountsReceived = ethValues.length > 0 && polygonValues.length > 0
+// var output = { success: allTokenAmountsReceived }
+var totalCirculating = 0
 // convert values
 if (allTokenAmountsReceived) {
   const allValues = ethValues.concat(polygonValues)
@@ -17,11 +16,11 @@ if (allTokenAmountsReceived) {
   // console.log(totalSumLocked)
   const totalSumLockedWholeNumber = totalSumLocked.divRound((new BN(10)).pow(new BN(18)))
   const maxSupply = (new BN(10)).pow(new BN(9))
-  const totalCirculating = maxSupply.sub(totalSumLockedWholeNumber)
-  console.log(totalCirculating.toString())
-  output.circulating = totalCirculating.toString()
+  totalCirculating = maxSupply.sub(totalSumLockedWholeNumber)
+  // console.log(totalCirculating.toString())
+  // output.circulating = totalCirculating.toString()
 }
-// console.log(output)
+console.log(totalCirculating.toString())
 
 export default function handler(req, res) { 
   try {
