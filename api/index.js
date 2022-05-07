841f92bae3bb0443d9cf7d66180678ae77dd46b9
Anders Schultz <mail@anders-schultz.dk>
2022-05-07 19:05:58 +0100

    testing api structure

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
-export default function handler(req, res) { // this function will be launched when the API is called.
+import { settings } from './settings.mjs'
+import { getValuesFromTokenContract } from './token-amounts.mjs';
+import BN from 'bn.js'
+
+const ethValues = await getValuesFromTokenContract(settings.Ethereum.TokenAddresses, 'Ethereum')
+const polygonValues = await getValuesFromTokenContract(settings.Polygon.TokenAddresses, 'Polygon')
+const allValues = ethValues.concat(polygonValues)
+
+var totalSumLocked = allValues.reduce((prev, curr) => {
+  return prev.add(new BN(curr))
+}, new BN('0'))
+
+const totalSumLockedWholeNumber = totalSumLocked.divRound((new BN(10)).pow(new BN(18)))
+const maxSupply = (new BN(10)).pow(new BN(9))
+const totalCirculating = maxSupply.sub(totalSumLockedWholeNumber)
+// console.log(totalCirculating.toString())
+
+export default function handler(req, res) { 
   try {
-    res.send({ success: true, message: 'it workds' }) // send the lyrics
+    res.send({ success: true, circulating: totalCirculating.toString() })
   } catch (err) {
     res.send(err) // send the thrown error
   }
